447b81c20821738676c76b95c072b486ba69e389
student <Junjib@csu.fullerton.edu>
2019-04-24 19:44:56 -0700

    Added text file, work on addToFront.

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,33 +1,93 @@
 #include "linkedlist.h"
 
-linkedlist::linkedlist() {
-
+linkedlist::linkedlist()
+{
+  head = nullptr;
 }
 
-linkedlist::~linkedlist() {
+linkedlist::~linkedlist()
+{
     // RECURSIVE
 }
 
-int linkedlist::count() {
+int linkedlist::count()
+{
     // RECURSIVE
+    if(head == nullptr)
+    {
+      return 0;
+    }
+    else
+    {
+      head = head->next;
+      return 1 + count();
+    }
 }
 
-void linkedlist::addToFront(int n) {
-
+void linkedlist::addToFront(int n)
+{
+  node *newNode = new node;
+  node *temp = nullptr;
+  if(head == nullptr)
+  {
+    newNode->data = n;
+    head = newNode;
+  }
+  else
+  {
+    newNode->data = n;
+    if(newNode->data < head->data)
+    {
+      newNode->next = head;
+      head = newNode;
+    }
+    if(newNode->data > head->data)
+    {
+      if(head->next == nullptr)
+      {
+        head->next = newNode;
+      }
+      else if(newNode->data < head->next->data)
+      {
+        temp = head->next;
+        head->next = newNode;
+        newNode->next = temp;
+      }
+    }
+  }
+  for(node *test = head; test != nullptr; test = test->next)
+  {
+    cout << test->data << " ";
+  }
+  cout << endl;
 }
 
-double linkedlist::average() {
+double linkedlist::average()
+{
     // uses sum and count functions
 }
 
-int linkedlist::sum() {
+int linkedlist::sum()
+{
     // RECURSIVE
+    int listSum = 0;
+    if(head == nullptr)
+    {
+      return listSum;
+    }
+    else
+    {
+      return listSum + sum();
+      head = head->next;
+    }
 }
 
-void linkedlist::writeInorder(string & file) {
+void linkedlist::writeInorder(string & file)
+{
 
 }
 
-void linkedlist::writeReversed(string & file) {
+void linkedlist::writeReversed(string & file)
+{
     // RECURSIVE
 }
--- main.cpp
+++ main.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 #include "linkedlist.cpp"
 
 using namespace std;
 
-void readFile(linkedlist & list, string & file) {
+void readFile(linkedlist & list, string & file)
+{
+  ifstream readData;
+  int number;
 
+  readData.open(file);
+  while(readData >> number)
+  {
+    list.addToFront(number);
+  }
+
+  readData.close();
+  cout << endl;
 }
 
-int main() {
+int main()
+{
+  string file = "list.txt";
+  linkedlist newList;
+
+  readFile(newList, file);
 
     return 0;
 }
